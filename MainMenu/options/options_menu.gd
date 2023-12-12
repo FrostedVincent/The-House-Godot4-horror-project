4a44fef51db2eb309f747b472f7b210c35610d90
VINCENT BÉRUBÉ <vinnyskibb@gmail.com>
2023-12-11 23:19:54 -0500

    options finished...

--- MainMenu/options/options_menu.gd
+++ MainMenu/options/options_menu.gd
@@ -2,12 +2,14 @@ class_name OptionsMenu
 extends Control 
 
 @onready var back = $MarginContainer/VBoxContainer/Back as Button
+@onready var settings_tab_container = $MarginContainer/VBoxContainer/Settings_Tab_Container as SettingsTabContainer
 
 signal back_options_menu
 
 
 func _ready():
 	back.button_down.connect(_on_back_pressed)
+	settings_tab_container.Back_Option_menu.connect(_on_back_pressed)
 	set_process(false)
 
 
